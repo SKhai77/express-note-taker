e98e796f653238bcca32607960c4e4e75505c533
SKhai77 <orionkhai@gmail.com>
2023-08-17 23:45:52 -0500

    Changed note-id and  noteId to id that fixed the delete button and saved notes to display when clicked

--- routes/notes.js
+++ routes/notes.js
@@ -15,11 +15,11 @@ notes.get('/notes', (req, res)=> {
 
 // GET Route for a specific note
 notes.get('/notes', (req, res) => {
-  const noteId = req.params.note_id;
+  const id = req.params.id;
   readFromFile('./db/db.json')
     .then((data) => JSON.parse(data))
     .then((json) => {
-      const result = json.filter((note) => note.note_id === noteId);
+      const result = json.filter((note) => note.id === id);
       return result.length > 0
         ? res.json(result)
         : res.json('No note with that ID');
@@ -27,19 +27,19 @@ notes.get('/notes', (req, res) => {
 });
 
 // DELETE Route for a specific note
-notes.delete('/notes/:note_id', (req, res) => {
-  const noteId = req.params.note_id;
+notes.delete('/notes/:id', (req, res) => {
+  const id = req.params.id;
   readFromFile('./db/db.json')
     .then((data) => JSON.parse(data))
     .then((json) => {
       // Make a new array of all notes except the one with the ID provided in the URL
-      const result = json.filter((note) => note.note_id !== noteId);
+      const result = json.filter((note) => note.id !==id);
 
       // Save that array to the filesystem
       writeToFile('./db/db.json', result);
 
       // Respond to the DELETE request
-      res.json(`Item ${noteId} has been deleted 🗑️`);
+      res.json(`Item ${id} has been deleted 🗑️`);
     });
 });
 
@@ -53,7 +53,7 @@ notes.post('/notes', (req, res) => {
     const newNote = {
       title,
       text,
-      note_id: uuidv4(),
+      id: uuidv4(),
     };
 
     readAndAppend(newNote, './db/db.json');
